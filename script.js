a6afb94c368179f215c7bdd9a382a6a12f4a34fc
viblo29 <dachivibliani29@gmail.com>
2024-11-10 23:33:55 +0400

    1st.

---
+++ script.js
@@ -0,0 +1,95 @@
+// 1
+const add = (a, b) => a + b;
+console.log(add(10, 3));
+
+// 2
+const minus = function (a, b) {
+  return a - b;
+};
+console.log(minus(105, 5));
+
+// 3
+const isEven = (num) => num % 2 === 0;
+console.log(isEven(10));
+
+// 4
+multiply(2, 3, displayResult);
+function multiply(num1, num2, callback) {
+  const result = num1 * num2;
+  callback(result);
+}
+function displayResult(result) {
+  console.log(result);
+}
+
+// 5
+const isPositive = (num) => num > 0;
+console.log(isPositive(1));
+
+// 6 -------------- gasasworebeli
+const double = function (n) {
+  return n * 2;
+};
+console.log(double(5));
+
+// 7
+doubleUp(10, displayOrmagiRicxvi);
+function doubleUp(ricxvi, callback) {
+  const result = ricxvi * 2;
+  callback(result);
+}
+function displayOrmagiRicxvi(result) {
+  console.log(result);
+}
+
+// 8
+const isDivisibleByThree = (num) => num % 3 === 0;
+console.log(isDivisibleByThree(9));
+
+//9
+thisNum(6, isEvenn)
+function thisNum(luwi,callback) {
+    const result = luwi % 2 === 0;
+    callback(result);
+}
+function isEvenn(result) {
+    console.log(result)
+}
+
+// 10
+const cube = (num) => num * num * num;
+console.log(cube(2));
+
+// 11
+multiplyy(2, 3, displayResult);
+function multiplyy(num1, num2, callback) {
+  const result = num1 * num2;
+  callback(result);
+}
+function displayResult(result) {
+  console.log(result);
+}
+
+// 12
+const isGreaterThanZero = (num) => num > 0;
+console.log(isGreaterThanZero(1));
+
+// 13 
+const cameti = function (n) {
+    return n/2;
+}
+console.log(cameti(10));
+
+// 14
+sum(9,1, displaySum)
+function sum(a, b, callback) {
+    const result = a + b;
+    callback(result);
+}
+function displaySum(result){
+    console.log(result)
+}
+
+// 15
+const square = (num) => num * num;
+console.log(square(3));
